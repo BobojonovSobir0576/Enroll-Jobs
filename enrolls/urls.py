1e03bc691ef7450c622dbc8533685680637ee357
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-11-29 05:08:38 -0500

    Commit

--- enrolls/urls.py
+++ enrolls/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('job-categories/', JobCategoriesView.as_view()),
     path('job-category-crud-views/<int:pk>/', JobCategoryCrudViews.as_view()),
     path('job-vacancies-deleile/', JobVacanciesView.as_view()),
-    path('job-vacancies-details-view/<int:pk>', JobVacanciesDetailsView.as_view()),
+    path('job-vacancies-details-view/<int:pk>/', JobVacanciesDetailsView.as_view()),
     path('jobs-vacancies', JobVacanciesAllView.as_view()),
     path('apply-job-user/', AppllyJobView.as_view()),
     path('apply-job-user-serach/', ApplySearchView.as_view()),
